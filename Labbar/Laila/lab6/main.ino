b1c733bb3307e0b5c875578aa0d7ba9e00208c2c
laila-93 <lailazai3@outlook.com>
2025-10-02 11:23:36 +0200

    lab6

---
+++ Labbar/Laila/lab6/main.ino
@@ -0,0 +1,23 @@
+/*
+#include <LiquidCrystal.h>
+
+// RS, E, D4, D5, D6, D7
+LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
+
+void setup() {
+  // starta LCD (16 kolumner, 2 rader)
+  lcd.begin(16, 2);
+
+  // skriv ut text på rad 1
+  lcd.setCursor(0, 0);
+  lcd.print("Hej Arduino!");
+
+  // skriv ut text på rad 2
+  lcd.setCursor(0, 1);
+  lcd.print("LCD fungerar :)");
+}
+
+void loop() {
+  // inget mer behövs, texten stannar kvar
+}
+*/
